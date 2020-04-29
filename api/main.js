0d0801491de7dafe7d0726d33c38c3f61a4639ab
Mathieu <mathieu.caselles@ynov.com>
2020-04-29 13:59:29 +0200

    maj front
    l'acceuil affiche désormais la liste des ôkémons ainsi que l

--- api/main.js
+++ api/main.js
@@ -1,58 +1,61 @@
-const express = require('express')
-const {Op, Sequelize} = require('sequelize');
+const express = require('express');
+const cors = require('cors');
+const { Op, Sequelize } = require('sequelize');
 
 
 const app = express();
 
+app.use(cors());
+
 const sequelize = new Sequelize('sqlite:pokedex.db', {
     define: {
         charset: 'utf8',
-        collate: 'utf8_general_ci', 
+        collate: 'utf8_general_ci',
         timestamps: true
-      },
+    },
 })
 
 const Pokemons = sequelize.define('pokemon',
-{
-    id: {
-        field: 'id',
-        type: Sequelize.INTEGER,
-        primaryKey: true
-    },
-    name: {
-        field: 'name',
-        type: Sequelize.STRING,
-    },
-    type1: {
-        field: 'type1',
-        type: Sequelize.STRING,
-    },
-    type2: {
-        field: 'type2',
-        type: Sequelize.STRING,
-    },
-}, {
+    {
+        id: {
+            field: 'id',
+            type: Sequelize.INTEGER,
+            primaryKey: true
+        },
+        name: {
+            field: 'name',
+            type: Sequelize.STRING,
+        },
+        type1: {
+            field: 'type1',
+            type: Sequelize.STRING,
+        },
+        type2: {
+            field: 'type2',
+            type: Sequelize.STRING,
+        },
+    }, {
     timestamps: false
 });
 
 const Types = sequelize.define('type',
-{
-    id: {
-        field: 'id',
-        type: Sequelize.INTEGER,
-        primaryKey: true
-    },
-    name: {
-        field: 'name',
-        type: Sequelize.STRING,
-    },
-}, {
+    {
+        id: {
+            field: 'id',
+            type: Sequelize.INTEGER,
+            primaryKey: true
+        },
+        name: {
+            field: 'name',
+            type: Sequelize.STRING,
+        },
+    }, {
     timestamps: false
 });
 
 
-app.get('/api/pokemons', function(req, res) {
-    Pokemons.findAll().then((pokemons) =>{
+app.get('/api/pokemons', function (req, res) {
+    Pokemons.findAll().then((pokemons) => {
         res.json(pokemons)
     })
 })
@@ -68,30 +71,30 @@ app.get('/api/pokemon/:numPokemon', function(req, res) {
     })
 })
 
-app.get('/api/pokemons/types/:type', function(req, res) {
+app.get('/api/pokemons/types/:type', function (req, res) {
     Pokemons.findAll({
         where: {
-          [Op.or]: [
-            Sequelize.where(Sequelize.fn('lower', sequelize.col('type1')), req.params.type),
-            Sequelize.where(Sequelize.fn('lower', sequelize.col('type2')), req.params.type),
-          ]
+            [Op.or]: [
+                Sequelize.where(Sequelize.fn('lower', sequelize.col('type1')), req.params.type),
+                Sequelize.where(Sequelize.fn('lower', sequelize.col('type2')), req.params.type),
+            ]
         }
-      }).then((pokemons) =>{
-          if (pokemons.length === 0){
-              res.json({error: "Le type entré n'est pas reconnu"})
-          }else
+    }).then((pokemons) => {
+        if (pokemons.length === 0) {
+            res.json({ error: "Le type entré n'est pas reconnu" })
+        } else
             res.json(pokemons)
-      })
+    })
 }),
 
-app.get('/api/types', function(req, res) {
-    Types.findAll().then((types) =>{
-        res.json(types)
+    app.get('/api/types', function (req, res) {
+        Types.findAll().then((types) => {
+            res.json(types)
+        })
     })
-})
 
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
     res.status(404).send("Page introuvable??")
-  });
+});
 
 app.listen(3000)
